e1eadbf98152ec928f6df6e606f9a51ee4d61b08
aparkening <aaron.parkening@gmail.com>
2019-07-23 11:55:53 -0400

    Done. Home links to new. New posts to display.

--- app.rb
+++ app.rb
@@ -2,4 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  # Homepage
+  get '/' do
+    erb :index
+  end
+
+  # Puppy list form
+  get '/new' do
+    erb :create_puppy
+  end
+
+  # Display puppy results
+  post '/puppy' do
+    pup = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = pup.name
+    @breed = pup.breed
+    @age = pup.months_old
+
+    erb :display_puppy
+  end
+
 end
